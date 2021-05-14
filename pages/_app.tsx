fd2226810ec800505b1725bf3ac7362c5929e71b
K-shir0 <kakishiro2190394@gmail.com>
2021-05-14 23:49:04 +0900

    feat: Amplifyのコンフィグを読み取るようにした

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import {AmplifyAuthenticator} from "@aws-amplify/ui-react";
+import {Amplify} from "aws-amplify";
+import awsExports from "../src/aws-exports";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+Amplify.configure({...awsExports, ssr: true});
+
+function MyApp({Component, pageProps}) {
+  return (
+    <AmplifyAuthenticator>
+      <Component {...pageProps} />
+    </AmplifyAuthenticator>
+  )
 }
 
 export default MyApp
