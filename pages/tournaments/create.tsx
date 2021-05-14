e7706d37d5b053bd2cce7a4b3854c692398e3107
K-shir0 <kakishiro2190394@gmail.com>
2021-05-15 03:37:49 +0900

    feat: 大会を作成するときに、フォームに入力された値をstateに格納する関数を作成

--- pages/tournaments/create.tsx
+++ pages/tournaments/create.tsx
@@ -2,31 +2,42 @@ import React, {useState} from "react";
 
 const CreateTournamentsPage: React.FC = () => {
   const [state, setState] = useState({
-    tournamentName: '',
-    startTime: '',
-    maximumNumberOfParticipants: '',
+    tournamentName: '', // 大会名
+    startTime: '', // 開示時間
+    maximumNumberOfParticipants: '', // 最大参加人数
   })
 
-  const handleInputChange = (e) => {
-    const name = e.target.name;
+  /**
+   * フォームに入力された値をstateに格納する関数
+   * @param e
+   */
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, value} = e.target;
 
+    setState({...state, [name]: value})
   }
 
   return (
     <>
       <h1>作成</h1>
-      <form>
+      <div>
         <label>大会名 :
-          <input type="text" name="tournamentName"/>
+          <input type="text" name="tournamentName"
+                 onChange={(e) => handleInputChange(e)}/>
         </label>
         <label>開始時間 :
-          <input type="datetime-local" name="startTime"/>
+          <input type="datetime-local" name="startTime"
+                 onChange={(e) => handleInputChange(e)}/>
         </label>
         <label>最大参加人数 :
-          <input type="number" name="maximumNumberOfParticipants"/>
+          <input type="number" name="maximumNumberOfParticipants"
+                 onChange={(e) => handleInputChange(e)}/>
         </label>
-        <button>作成</button>
-      </form>
+        <button onClick={() => {
+          console.log(state)
+        }}>作成
+        </button>
+      </div>
     </>
   )
 }
